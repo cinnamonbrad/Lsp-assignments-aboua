1bdf0d8129e358144381af8e7512c3ae5fb97a1e
BA <BA@localhost>
2022-02-03 10:12:24 -0500

    Hello World

--- large-scale-programming-project/src/org/howard/edu/lsp/homework1/HelloWorld.java
+++ large-scale-programming-project/src/org/howard/edu/lsp/homework1/HelloWorld.java
@@ -3,7 +3,7 @@ package org.howard.edu.lsp.homework1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		system.out.println("hello world!")
+		System.out.println("hello world!");
 
 	}
 
